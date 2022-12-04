43cbbbde1df407c39dcda67787e8efa54b39c463
Lee Crosdale <accounts@leecrosdale.com>
2022-12-04 10:44:22 +0000

    Day 4

---
+++ day4/day4.py
@@ -0,0 +1,47 @@
+filename = './day4/data.txt'
+
+
+def run():
+    pairs = 0
+    overlaps = 0
+    with open(filename) as fh:
+        for line in fh:
+            print("=============")
+            print(line)
+            line = line.strip('\n')
+            ranges = line.split(',')
+            first = ranges[0].split('-')
+            second = ranges[1].split('-')
+
+            print(f"First: {first}")
+            print(f"Second: {second}")
+
+            first0 = int(first[0])
+            first1 = int(first[1])
+            second0 = int(second[0])
+            second1 = int(second[1])
+
+            if first0 >= second0 and first1 <= second1:
+                pairs += 1
+                print("PAIR")
+            elif second0 >= first0 and second1 <= first1:
+                pairs += 1
+                print("PAIR")
+
+
+            if second0 <= first0 <= second1:
+                print("Overlap")
+                overlaps += 1
+            elif second0 <= first1 <= second1:
+                print("Overlap")
+                overlaps += 1
+            elif first0 <= second0 <= first1:
+                print("Overlap")
+                overlaps += 1
+            elif first0 <= second1 <= first1:
+                print("Overlap")
+                overlaps += 1
+
+
+    print(f"Total Pairs: {pairs}")
+    print(f"Total Overlaps: {overlaps}")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import day1.day1
 import day2.day2
 import day3.day3
+import day4.day4
 
 if __name__ == '__main__':
     # day1.day1.run() # Day 1
     # day2.day2.run_part1() # Day 2 Part 1
     # day2.day2.run_part2() # Day 2 Part 2
-    day3.day3.run()
+    # day3.day3.run()
+    day4.day4.run()
